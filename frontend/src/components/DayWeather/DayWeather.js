39e66a1cab5e45015b8ae1c9a5c66003c9dac959
Heidrun Heidarsdottir <heidrunh17@ru.is>
2020-10-08 01:05:13 +0200

    Fixed date to weekday conversion

--- frontend/src/components/DayWeather/DayWeather.js
+++ frontend/src/components/DayWeather/DayWeather.js
@@ -5,23 +5,25 @@ const DayWeather = (props) => {
   const conditions = day.merki;
   const temp = day.t2 + "°C";
   const wind = day.f10 + " m/s";
-  const weekdayInt = new Date(day.dags_spar).getUTCDay(); // 0 = Monday, 6 = Sunday
+  const weekdayInt = new Date(day.dags_spar).getUTCDay(); // 0 = Sunday, 6 = Saturday
+
   let weekday = "";
   if (weekdayInt === 0) {
-    weekday = "mánudagur";
-  } else if (weekdayInt === 1) {
-    weekday = "þriðjudagur";
-  } else if (weekdayInt === 2) {
-    weekday = "miðvikudagur";
-  } else if (weekdayInt === 3) {
-    weekday = "fimmtudagur";
-  } else if (weekdayInt === 4) {
-    weekday = "föstudagur";
-  } else if (weekdayInt === 5) {
-    weekday = "laugardagur";
-  } else if (weekdayInt === 6) {
     weekday = "sunnudagur";
+  } else if (weekdayInt === 1) {
+    weekday = "mánudagur";
+  } else if (weekdayInt === 2) {
+    weekday = "þriðjudagur";
+  } else if (weekdayInt === 3) {
+    weekday = "miðvikudagur";
+  } else if (weekdayInt === 4) {
+    weekday = "fimmtudagur";
+  } else if (weekdayInt === 5) {
+    weekday = "föstudagur";
+  } else if (weekdayInt === 6) {
+    weekday = "laugardagur";
   }
+  console.log("WEEKDAY: ", weekdayInt, ": ", weekday);
   return (
     <>
       <p>{weekday}</p>
