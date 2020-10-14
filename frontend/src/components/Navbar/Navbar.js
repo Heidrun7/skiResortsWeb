6390bd365acb866ee8acab40c6fc7e782c7f3cd4
Heidrun Heidarsdottir <heidrunh17@ru.is>
2020-10-15 00:13:18 +0200

    Styled Navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,38 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
+import logo from "../../images/fullLogoWhite.png";
 
 const Navbar = () => {
   return (
-    <>
-      <Link
-        to={{
-          pathname: "/",
-        }}
-      >
-        Heim
-      </Link>
-      {/* <Link
-        to={{
-          pathname: "/iceland",
-        }}
-      >
-        Iceland
-      </Link>
-      <Link
-        to={{
-          pathname: "/worldwide",
-        }}
-      >
-        Worldwide
-      </Link> */}
-      <Link
-        to={{
-          pathname: "/about",
-        }}
-      >
-        Um síðuna
-      </Link>
-    </>
+    <div className="nav">
+      <div className="home">
+        <Link
+          to={{
+            pathname: "/",
+          }}
+        >
+          <img src={logo} alt="Besta veðrið" />
+        </Link>
+      </div>
+      <div className="about">
+        <Link
+          to={{
+            pathname: "/about",
+          }}
+        >
+          Um síðuna
+        </Link>
+      </div>
+    </div>
   );
 };
 
