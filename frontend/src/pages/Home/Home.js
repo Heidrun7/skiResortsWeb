28e26e1f260a88698796d23f216d0fb642f39932
Heidrun Heidarsdottir <heidrunh17@ru.is>
2020-12-10 01:59:39 +0000

    Added moving skier on load of Home page

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,15 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import BestToday from "../BestToday/BestToday";
 import BestThisWeek from "../BestThisWeek/BestThisWeek";
+import skier from "../../images/skier/skier_white.png";
 import "./Home.css";
 
 const Home = () => {
   const [id, setId] = useState(988);
+  const [displayOverlay, setDisplayOverlay] = useState("block");
+  const [displayHome, setDisplayHome] = useState("none");
 
   const handleResortChange = (e) => {
     setId(e.target.value);
   };
+
+  useEffect(() => {
+    setTimeout(function () {
+      setDisplayOverlay("none");
+      setDisplayHome("grid");
+    }, 3000);
+  }, []);
 
   let select = (
     <select onChange={(e) => handleResortChange(e)} defaultValue={"988"}>
@@ -26,23 +36,28 @@ const Home = () => {
   );
 
   return (
-    <div className="homeContainer">
-      <div className="today">
-        <div className="center">
-          <div className="content">
-            <BestToday />
-            {/* <Link to="/dagur">
+    <div className="wrapper">
+      <div className="overlay" style={{ display: displayOverlay }}>
+        <img src={skier} alt="Skier" className="skier"></img>
+      </div>
+      <div className="homeContainer" style={{ display: displayHome }}>
+        <div className="today">
+          <div className="center">
+            <div className="content">
+              <BestToday />
+              {/* <Link to="/dagur">
               <>Sjá annan dag</>
             </Link> */}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="thisWeek">
-        <div className="center">
-          <div className="content">
-            <BestThisWeek id={id} select={select} />
-            <div className="seeWeek">
-              <Link to="/vikuspa">Sjá vikuspá</Link>
+        <div className="thisWeek">
+          <div className="center">
+            <div className="content">
+              <BestThisWeek id={id} select={select} />
+              <div className="seeWeek">
+                <Link to="/vikuspa">Sjá vikuspá</Link>
+              </div>
             </div>
           </div>
         </div>
